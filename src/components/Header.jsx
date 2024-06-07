d22137f5f43698cb03d40bc130813796ce081e4b
Chokri Yassine <123024506+ChokriYassine143@users.noreply.github.com>
2024-06-07 16:37:16 +0100

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -27,19 +27,19 @@ const Header = () => {
                 <nav>
                     <ul className="menu">
                         <li><a href="/">Acceuil</a></li>
-                        <li><a href="/apropos">Apropos</a></li>
-                        <li><a href="/formations">Formation</a></li>
+                        <li><a href="https://wikispace.onrender.com//apropos">Apropos</a></li>
+                        <li><a href="https://wikispace.onrender.com//formations">Formation</a></li>
                         <li className="dropdown" onMouseLeave={closeDropdown}>
                             <button onClick={toggleDropdown} className="dropdown-button">Coworking</button>
                             {isDropdownOpen && (
                                 <div className="dropdown-content">
-                                    <a href="/cowrking" onClick={handleDropdownClick}>Coworking</a>
-                                    <a href="/Domiciliation" onClick={handleDropdownClick}>Domiciliation</a>
+                                    <a href="https://wikispace.onrender.com//cowrking" onClick={handleDropdownClick}>Coworking</a>
+                                    <a href="https://wikispace.onrender.com//Domiciliation" onClick={handleDropdownClick}>Domiciliation</a>
                                     <a href="#" onClick={handleDropdownClick}>Événement</a>
                                 </div>
                             )}
                         </li>
-                        <li><a href="/contact">Contact</a></li>
+                        <li><a href="https://wikispace.onrender.com//contact">Contact</a></li>
                     </ul>
                 </nav>
                 <img src={logo2} alt="Logo 2" className="logo" />
