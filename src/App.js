4266a2b430fbaacd1396f156a4985483ddc1a867
Chokri Yassine <123024506+ChokriYassine143@users.noreply.github.com>
2024-06-07 17:04:45 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,22 +3,22 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Acceuil from './components/Acceuil';
 import ContactFormPage from './components/Contact';
 import Apropos from './components/Apropos';
-import Domiciliation from './components/Domiciliation'; // Import Domiciliation component
+import Domiciliation from './components/Domiciliation';
 import Coworking from './components/Coworking';
 import Formation from './components/Formations';
 import FormationDetails from './components/FormationDetails';
+
 const App = () => {
     return (
         <Router>
             <Routes>
-                <Route path="/" exact element={<Acceuil />} />
+                <Route path="/" element={<Acceuil />} />
                 <Route path="/contact" element={<ContactFormPage />} />
                 <Route path="/apropos" element={<Apropos />} />
-                <Route path="/domiciliation" element={<Domiciliation />} /> 
+                <Route path="/domiciliation" element={<Domiciliation />} />
                 <Route path="/coworking" element={<Coworking />} />
-                <Route path="/formations" element={<Formation />} /> 
-                <Route path="/FormationDetails" element={<FormationDetails />} /> 
-                 {/* Add Route for Domiciliation component */}
+                <Route path="/formations" element={<Formation />} />
+                <Route path="/FormationDetails" element={<FormationDetails />} />
             </Routes>
         </Router>
     );
